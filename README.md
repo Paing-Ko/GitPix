000838836098f331ac3a5ab057cd610cd96929f3
Paing Ko <paingphyoko.keke@gmail.com>
2023-10-20 11:45:40 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# 🐙 GitPix
+
+Welcome to `GitFix`! A fun and interactive application that allows you to get a fresh perspective on GitHub users by mashing up data from Unsplash, GitHub, and GIPHY API.
+
+![GitPix App Screenshot](media/Screenshot%202023-10-20%20at%2010.29.50.png)
+
+<!-- You can replace this with a real screenshot of your app -->
+
+🔗 [Try GitPix](https://paing-ko.github.io/GitPix/)
+
+## 🌟 Features
+
+- **Search GitHub Users:** Input a GitHub username and get a visually appealing profile with data from GitHub.
+- **Dynamic Backgrounds:** With each profile, enjoy a unique background fetched from Unsplash.
+- **User Info:** View the user's repos, followers, following, stars, contributions, preferred languages, and more.
+- **Interactive Charts:** Get insights into user data with intuitive charts.
+- **GIPHY Search:** Fancy a laugh or want to express an emotion? Search and view GIPHYs right at the bottom.
+
+## 🧠 User Stories
+
+### Core Stories
+
+- **Mashup of Data:** See an intriguing mix of images, GitHub user data, and GIFs.
+- **User Interaction:** Input a GitHub username or search a GIPHY to change the displayed result.
+- **Responsive Design:** Seamlessly view the app across all devices.
+
+### Stretch Stories
+
+- **Data Loading Indication:** As data loads, users get a visual cue indicating the process.
+- **Error Handling:** Users are promptly informed when something doesn't go as planned.
+
+## ✔️ Acceptance Criteria
+
+- Queries at least two APIs using `fetch`.
+- Dynamic content, all generated using JS.
+- Well-documented user journey.
+- Mobile-first, responsive design.
+- Accessibility considered and implemented for a broad range of users.
+
+## 🛠️ Planning
+
+During the planning phase, I:
+
+- Researched and chose APIs that fit my app vision.
+- Carefully considered the user's journey through the app.
+- Defined MVP and started working on data fetching then styling.
+
+## 🔑 API Keys
+
+For this project, I've utilized APIs that are either token-free or employ free API keys. In a real-world scenario, always avoid exposing API keys on the client side.
+- **Unsplash**: Provides the vast library of diverse images. 
+  - [View Documentation](https://unsplash.com/documentation)
+- **GitHub**: Powers the core feature of fetching GitHub user data.
+  - [View Documentation](https://docs.github.com/en/rest)
+- **GIPHY**: The source of the entertaining GIFs that users can search for.
+  - [View Documentation](https://developers.giphy.com/docs/api/)
+
+
+## 🚀 Getting Started
+
+1. Clone the repo:
+   ```bash
+   git clone https://github.com/Paing-Ko/GitPix.git
+   ```
+2. Navigate into the directory:
+
+   ```bash
+   cd GitPix
+   ```
+
+3. Open index.html in your browser.
+
+   ```bash
+   open index.html
+   ```
+
+4. Start exploring and have fun!
---
+++ script.js
@@ -0,0 +1,209 @@
+document
+  .getElementById("username")
+  .addEventListener("keydown", function (event) {
+    if (event.keyCode === 13) fetchRepos();
+  });
+
+async function fetchRepos() {
+  const username = document.getElementById("username").value;
+  if (!username) return alert("Please enter a GitHub username.");
+
+  try {
+    const [profile, repos] = await Promise.all([
+      fetch(`https://api.github.com/users/${username}`).then((res) =>
+        res.json()
+      ),
+      fetch(`https://api.github.com/users/${username}/repos?sort=updated`).then(
+        (res) => res.json()
+      ),
+    ]);
+
+    // Handling user not found
+    if (profile.message && profile.message.includes("Not Found")) {
+      return alert("GitHub user not found.");
+    }
+
+    // Default values for null fields
+    profile.name = profile.name || "Unknown Name";
+    profile.bio = profile.bio || "No bio available";
+    profile.login = profile.login || "Unknown Username";
+    profile.avatar_url = profile.avatar_url || ""; // replace with a default image URL if you prefer
+
+    const flickButton = document.getElementById("flickBtn");
+    flickButton.classList.add("flipped");
+
+    // Optional: Remove the flipped class after the animation completes to reset the effect
+    flickButton.addEventListener("animationend", () => {
+      flickButton.classList.remove("flipped");
+    });
+
+    const repoContainer = document.getElementById("repoContainer");
+    const totalStars = repos.reduce(
+      (sum, repo) => sum + repo.stargazers_count,
+      0
+    );
+
+    repoContainer.innerHTML = `
+    <div id="profile-background">
+        <div id="profile-container">
+            <img src="${profile.avatar_url}" class="profile-pic" alt="${username}'s Profile Picture">
+            <h2>${profile.name}</h2>
+            <span class="username">${profile.login}</span>
+            <p>${profile.bio}</p>
+            <button class="follow-btn" onclick="window.open('${profile.html_url}', '_blank')">Follow</button>
+        </div>
+    </div>
+    <div class="stats-container">
+        <div class="stats-item">Repos:  ${profile.public_repos}</div>
+        <div class="stats-item">Followers:  ${profile.followers}</div>
+        <div class="stats-item">Following:  ${profile.following}</div>
+        <div class="stats-item">⭐:  ${totalStars} star(s)</div>
+    </div>
+
+     <div class="repo-box">
+        <div class="repo-header">Recent Repos</div>
+        <div class="repo-list">
+        </div>
+    </div>
+`;
+
+    const repoListDiv = repoContainer.querySelector(".repo-list");
+    // Profile pic rotate
+    const profilePic = document.querySelector(".profile-pic");
+    // Function to start rotation
+    function startRotation() {
+      profilePic.style.animation = "rotate 1s linear infinite";
+    }
+    // Function to stop rotation and reset animation
+    function stopRotation() {
+      profilePic.style.animation = "";
+      void profilePic.offsetWidth; // This is a trick to reset the animation
+    }
+
+    // For touch devices
+    profilePic.addEventListener("touchstart", startRotation);
+    profilePic.addEventListener("touchend", stopRotation);
+
+    // For non-touch devices
+    profilePic.addEventListener("mousedown", startRotation);
+    profilePic.addEventListener("mouseup", stopRotation);
+    profilePic.addEventListener("mouseleave", stopRotation);
+
+    // For hover effect
+    profilePic.addEventListener("mouseenter", startRotation);
+    profilePic.addEventListener("mouseout", stopRotation);
+
+    for (let i = 0; i < 2 && i < repos.length; i++) {
+      const repo = repos[i];
+      const repoDiv = document.createElement("div");
+      repoDiv.innerHTML = `<h2>${repo.name}</h2><a href="${repo.html_url}" target="_blank">View on GitHub</a>`;
+      repoListDiv.appendChild(repoDiv);
+    }
+
+    displayImages(username);
+    await setBackground();
+  } catch (error) {
+    console.error("Network Error:", error);
+  }
+}
+
+// generate charts
+function displayImages(username) {
+  const images = [
+    {
+      id: "top-langs",
+      url: `https://github-readme-stats.vercel.app/api/top-langs?username=${username}&show_icons=true&locale=en&layout=compact`,
+    },
+    {
+      id: "user-stats",
+      url: `https://github-readme-stats.vercel.app/api?username=${username}&show_icons=true&locale=en`,
+    },
+    {
+      id: "streak-stats",
+      url: `https://github-readme-streak-stats.herokuapp.com/?user=${username}`,
+    },
+    {
+      id: "contribution-graph",
+      url: `https://ghchart.rshah.org/40916c/${username}`,
+    },
+  ];
+
+  images.forEach((img) => {
+    const imageElement = document.getElementById(img.id);
+    imageElement.style.display = "block";
+    imageElement.src = img.url;
+  });
+}
+
+// profile background
+async function setBackground() {
+  const response = await fetch(
+    "https://api.unsplash.com/photos/random?query=programming&client_id=9-VX4xJ6eSMuRCnKpGQP_zyM_QqSgKtmvHZmUR4DTYs"
+  );
+  const data = await response.json();
+  const imageUrl = data.urls.full;
+  document.getElementById(
+    "profile-background"
+  ).style.cssText = `background-image: url(${imageUrl});
+`;
+}
+
+// Rainbow
+const spans = document.querySelectorAll(".rainbow span");
+
+spans.forEach((span, index) => {
+  setTimeout(() => {
+    span.style.animationDelay = `${index * 0.2}s`;
+  }, 0);
+});
+
+// GIPHY
+async function fetchGif(searchTerm) {
+  const response = await fetch(
+    `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=7zcr5tNW9JmYSO2awsqBqGK3OjpLjnh3&limit=10`
+  );
+  const data = await response.json();
+  const randomIndex = Math.floor(Math.random() * data.data.length);
+  return data.data[randomIndex].images.original.url;
+}
+
+const modal = document.getElementById("giphyModal");
+const closeBtn = document.querySelector(".close");
+
+function searchGif() {
+  const searchTerm = document.getElementById("search-term").value.trim(); // .trim() removes whitespace from the start and end
+
+  // Check if searchTerm is empty or not
+  if (!searchTerm) {
+    alert("Please enter a search term before searching.");
+    return; // Exit the function early
+  }
+
+  fetchGif(searchTerm).then((url) => {
+    const giphyImage = document.getElementById("giphy-image");
+    giphyImage.src = url;
+    giphyImage.style.display = "block"; /* Display the image element */
+    modal.style.display = "block"; // Display the modal only after a successful search
+  });
+
+  document.getElementById("search-term").value =
+    ""; /* Clear the input element's value */
+}
+
+function checkEnter(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    searchGif();
+  }
+}
+
+// Function to close the modal
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+};
+
+
+// logo reload
+document.getElementById("logo").addEventListener("click", function () {
+  window.location.reload();
+});
